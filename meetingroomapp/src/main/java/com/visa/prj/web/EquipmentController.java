9549fec7d543bd52a126024666311aeb1ad7ef3d
Ayush Bhargava <ayushtoo@gmail.com>
2018-08-09 11:35:18 +0100

    UserRoomSetup page added

--- meetingroomapp/src/main/java/com/visa/prj/web/EquipmentController.java
+++ meetingroomapp/src/main/java/com/visa/prj/web/EquipmentController.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.visa.prj.entity.Equipment;
 import com.visa.prj.service.AdminService;
 
+@RestController
 public class EquipmentController {
 	
 	@Autowired
